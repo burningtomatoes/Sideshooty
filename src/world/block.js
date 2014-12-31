252b297ccae7e37e888be3c6479573d797a3a906
Roy de Jong <roy@softwarepunt.nl>
2014-12-31 21:33:25 +0100

    Blocks: Add support for collision detection debug drawing, rectangle determination for collision detection

--- src/world/block.js
+++ src/world/block.js
@@ -3,13 +3,37 @@ var Block = function(x, y) {
 
     this.txBody = new Image();
     this.txBody.src = 'assets/textures/brickwall.png';
+    this.renderSize = { w: 16, h: 16 };
+
+    this.isBlock = true;
 
     this.draw = function(ctx) {
-        ctx.drawImage(this.txBody, 0, 0, 16, 16, Camera.translateX(this.position.x),
-            Camera.translateY(this.position.y), 16, 16);
+        ctx.drawImage(this.txBody, 0, 0, this.renderSize.w, this.renderSize.h, Camera.translateX(this.position.x),
+            Camera.translateY(this.position.y), this.renderSize.w, this.renderSize.h);
+
+        if (window.DEBUG_PROJECTILES) {
+            var sq = this.getRect();
+
+            ctx.beginPath();
+            ctx.rect(Camera.translateX(sq.left), Camera.translateY(sq.top), sq.width, sq.height);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = 'red';
+            ctx.stroke();
+        }
     };
 
     this.update = function() {
 
+    };
+
+    this.getRect = function() {
+        return {
+            top: this.position.y,
+            left: this.position.x,
+            height: this.renderSize.h,
+            width: this.renderSize.w,
+            bottom: this.position.y + this.renderSize.h,
+            right: this.position.x + this.renderSize.w
+        };
     };
 };
